a5bbfa983da8c2c8922cb258560c5fa766a2891e
taylor <taylor@splashthat.com>
2015-07-24 17:30:17 -0400

    working email subscription

--- js/subscribe-user.js
+++ js/subscribe-user.js
@@ -4,10 +4,10 @@ $(function() {
   $('#subscribe-me').click( subscribeUser );
 });
 
-function subscribeUser() {
+function subscribeUser(e) {
+  e.preventDefault();
   userEmail = $('#subscriber-email').val();
 
-  // if isValidEmail(userEmail) {
   var Subscription = Parse.Object.extend("Subscription");
   var subscription = new Subscription();
   subscription.save({email: userEmail}, {
@@ -20,15 +20,4 @@ function subscribeUser() {
       console.log(userEmail);
     }
   });
-  // }
-}
-
-function isValidEmail(e) {
-  at = e.split('@')
-  if (at.length < 2) { return false; }
-
-  domain = at[1].split('.');
-  if (domain[domain.length-1] != edu) { return false; }
-
-  return true;
 }
