1e7d37bfd44cf8239df9b362a6c1506efeb666fc
taylor <taylor@splashthat.com>
2015-11-23 19:43:07 -0500

    some css changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 $(function() {
 
     init();
+
+    //nav links: go to according pane
+    $('#breadcrumbs li').click( function() {
+        var id = $(this).attr('id');
+        activatePane(id);
+        scrollToPane(id);
+
+    });
+
     $(window).resize(init);
-    $(window).scroll(headerFix);
+    $(window).scroll(scrollInit);
 });
 
 function init() {
@@ -10,12 +19,17 @@ function init() {
     centerSplashContent();
 }
 
+function scrollInit() {
+    headerFix();
+    updateActive();
+}
+
 function splashPaneFullHeight() {
     var winHeight = $(window).height();
     var winWidth = $(window).width();
     var navHeight = $('#breadcrumbs').outerHeight();
     var $splash = $('#splash');
-    if (winWidth > 750) {
+    if (winWidth > 767) {
         $splash.css('min-height', winHeight-navHeight);
     } else {
         $splash.css('min-height', 'auto');
@@ -45,5 +59,35 @@ function headerFix() {
     } else {
         $nav.removeClass('fixed');
         $splash.css('margin-bottom', 0);
+    }
+}
+
+//nav links: update depending on scroll
+function scrollToPane(pane) {
+
+    var $nav = $('#breadcrumbs');
+    var newPos = $('.pane.' + pane).offset().top;
+
+    $('html, body').animate({
+        scrollTop: newPos - $nav.outerHeight()
+    }, 500);
+}
+
+function activatePane(id) {
+    $('#breadcrumbs .active').removeClass('active');
+    $('#' + id).addClass('active');
+}
+
+function updateActive() {
+    var pos = $(window).scrollTop();
+    console.log(pos);
+    var panes = $('.pane');
+    for ( p in panes ) {
+        var panePos = p.offset().top;
+        if (panePos < pos) {
+            var id = p.attr('id');
+            activatePane(id);
+        }
+
     }
 }
