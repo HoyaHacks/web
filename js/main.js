3f0e382feb1841006184a6639992bff23b3aed72
taylor <taylor@splashthat.com>
2016-07-20 22:30:25 -0700

    sidebar is fixed and mobile responsiveness

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(function() {
 
-	centerBlock($(".main-content"));
+	centerBlock($(".sidebar .wrapper"));
 
 	$(window).resize(function() {
-		centerBlock($(".main-content"));
+		centerBlock($(".sidebar .wrapper"));
 	});
 
 });
@@ -13,8 +13,13 @@ function centerBlock($b) {
 	var blockHeight = $b.outerHeight();
 	var offset = (height - blockHeight) * 0.35;
 	if (offset > 0) {
-		$b.attr("style", "margin-top: " + offset + "px;");
+		$b.attr("style", "padding-top: " + offset + "px; padding-bottom: " + offset + "px");
 	} else {
-		$b.attr("style", "margin-top: 0;");
+		$b.attr("style", "padding-top: 0; padding-bottom: 0");
 	}
+}
+
+function fullHeight($b) {
+	var height = $(window).height();
+	$b.attr("style", "min-height: " + height + "px");
 }
